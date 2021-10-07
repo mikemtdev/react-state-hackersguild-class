16dc0ac2b03ebceff8e255815d38ccd66a8341f3
Mike M.T Njovu <mikemnjovu@gmail.com>
2021-10-07 09:47:20 +0200

    Initial commit

---
+++ src/components/ui/HelloWorld/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World </h1>
+      <h2>Fast CRA Template </h2>
+    </div>
+  )
+}
+
+export default HelloWorld
---
+++ src/container/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HelloWorld from '../../components/ui/HelloWorld/HelloWorld'
+
+const HomePage = () => {
+  return (
+    <div>
+     <HelloWorld />
+    </div>
+  )
+}
+
+export default HomePage
